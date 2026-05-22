4ec03e59919248aca3ebb2a4f71e1e6a34e9f8bc
최문경 <pinkmk3127@gmail.com>
2026-05-22 15:22:11 +0900

    Update a.py

--- .github/ISSUE_TEMPLATE/a.py
+++ .github/ISSUE_TEMPLATE/a.py
@@ -1,1 +1,10 @@
+from datasets import load_dataset
 
+data = load_dataset('jeanlee/kmhas_korean_hate_speech')
+
+train = data['train']       # 78,977건
+valid = data['validation']  # 8,776건
+test  = data['test']        # 21,939건
+
+# pandas DataFrame으로 변환
+df_train = data['train'].to_pandas()
